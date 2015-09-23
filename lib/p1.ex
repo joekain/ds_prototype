c8142dae81c7a123004a5f1f0e1c3d9a285c3ac1
Joseph Kain <joekain@gmail.com>
2015-09-23 15:10:01 -0700

    Deadlocking but more concurrent version

--- lib/p1.ex
+++ lib/p1.ex
@@ -5,13 +5,21 @@ defmodule P1 do
 
   defp start_task(record) do
     Task.async(fn ->
-      Parser.urls(record) |> Enum.map(fn x -> Unshortener.expand(x) end)
+      Parser.urls(record) |> Enum.map(fn x ->
+        Unshortener.expand(x)
+      end)
     end)
   end
 
+  defp await_tasks(list_of_tasks) do
+    list_of_tasks
+    |> Enum.map(fn task -> Task.await(task, :infinity) end)
+  end
+
   def stream do
-    Fetcher.fetch  # Stream of tweets
-    |> Stream.map(fn x -> start_task(x) end)  # Stream of `Task`s
-    |> Stream.flat_map(fn x -> Task.await(x) end) # Stream of results - arrays of URLs
+    Fetcher.fetch  # Stream of Tweets
+    |> Stream.map(fn x -> start_task(x) end)  # Stream of `Task`s each processing a Tweet
+    |> Stream.chunk(5)  # Stream of lists with 5 `Tasks` each
+    |> Stream.flat_map(&await_tasks/1) # Stream of resulting URLs
   end
 end
