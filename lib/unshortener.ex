54849f9c6fa8d53b9214d3d3336643822a59b16d
Joseph Kain <joekain@gmail.com>
2015-09-23 11:02:48 -0700

    Handle unshortening a few more cases
    * http://fb.me/6RK0qYjJI
      * Handle Facebooks 302 redirect to unsupportedbrowser page
    * http://ow.ly/ScJ9I
      * Redirects to Amazon which doesn't support HEAD operation.  For now, assume all
        shortening services support HEAD and assume page that doesn't support HEAD is the target.

--- lib/unshortener.ex
+++ lib/unshortener.ex
@@ -8,19 +8,37 @@ defmodule Unshortener do
   end
 
   defp location(headers) do
-    {_key, value} = Enum.find(headers, nil, fn {key, value} ->
+    {_key, value} = Enum.find(headers, nil, fn {key, _value} ->
       canonical_field_name(key) == "location"
     end)
 
     value
   end
 
+  defp is_special_case(location) do
+    location in [
+      "https://www.facebook.com/unsupportedbrowser"  # Facebook redirects HTTPotion to the unsupported browser page
+    ]
+  end
+
+  defp handle_redirect(short, headers) do
+    new_location = location(headers)
+    case is_special_case(new_location) do
+      :true -> short
+      :false -> expand(new_location)
+    end
+  end
+
   @spec expand(String.t) :: String.t
   def expand(short) do
     try do
       case HTTPotion.head(short) do
-        %HTTPotion.Response{headers: headers, status_code: 301} -> location(headers) |> expand
-        %HTTPotion.Response{headers: headers, status_code: success} when 200 <= success and success < 300 -> short
+        %HTTPotion.Response{headers: headers, status_code: redirect} when 301 <= redirect and redirect <= 302 ->
+          handle_redirect(short, headers)
+        # Assumes all shortening services respond to HEAD and that target URLs may not.
+        # In that case expansion is done.
+        %HTTPotion.Response{headers: _headers, status_code: 405} -> short
+        %HTTPotion.Response{headers: _headers, status_code: success} when 200 <= success and success < 300 -> short
       end
     rescue
       _ -> :error
--- test/unshortener_test.exs
+++ test/unshortener_test.exs
@@ -16,4 +16,12 @@ defmodule UnshortenerTest do
   test "it can unshorten double shortened URLs" do
     assert Unshortener.expand("http://t.co/kfLrRZJ1cI") == "http://learningelixir.joekain.com/how-I-learned-elixir/?utm_content=buffer9a56c&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer"
   end
+
+  test "it can unshorten http://ow.ly/ScJ9I" do
+    assert Unshortener.expand("http://ow.ly/ScJ9I") == "http://www.amazon.fr/gp/product/B00XY4ZIRE/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&camp=1642&creative=6746&creativeASIN=B00XY4ZIRE&linkCode=as2&tag=petbuzmar-21"
+  end
+
+  test "it can unshorten http://fb.me/6RK0qYjJI" do
+    assert Unshortener.expand("http://fb.me/6RK0qYjJI") == "https://www.facebook.com/photo.php?fbid=863512007031067"
+  end
 end
