40a23e03430e69b429b319f01f915df100eb8f33
Joseph Kain <joekain@gmail.com>
2015-09-26 21:02:04 -0700

    Use Processor
    Using the new Processor module allows me to decouple fetch of tweets from
    URL unshortening.  This in turn, allows better async network behavior in which
    waiting for URL unshortening requests can happen concurrently.

--- lib/p1.ex
+++ lib/p1.ex
@@ -3,25 +3,11 @@ defmodule P1 do
     [ FetcherSingle.fetch |> Parser.urls |> Unshortener.expand ]
   end
 
-  defp start_task(record) do
-    Task.async(fn -> Unshortener.expand(record) end)
-  end
-
-  defp start_tasks(records) do
-    records |> Enum.map(&start_task/1)
-  end
-
-  defp await_tasks(list_of_tasks) do
-    list_of_tasks
-    |> Enum.map(fn task -> Task.await(task, :infinity) end)
+  def lazy_urls do
+    Fetcher.fetch |> Stream.flat_map(fn tweet -> Parser.urls(tweet) end)
   end
 
   def stream do
-    Fetcher.fetch  # Stream of Tweets
-    |> Stream.flat_map(&Parser.urls(&1))
-    |> Stream.chunk(5)  # Stream of lists with 5 Tweets each
-    |> Stream.flat_map(&start_tasks/1) # Stream of `Tasks`
-    |> Stream.chunk(5)  # Stream of lists with 5 `Tasks` each
-    |> Stream.flat_map(&await_tasks/1) # Stream of resulting URLs
+    Processor.map(&lazy_urls/0, fn url -> Unshortener.expand(url) end)
   end
 end
